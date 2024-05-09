48f90764565bc9d06a5313967b184625b9951230
RichardPerrota <160658684+RichardPerrota@users.noreply.github.com>
2024-05-09 17:09:13 -0300

    Update SplashActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-
+//esse código configura a interface do usuário da atividade Splash para exibir o conteúdo definido no arquivo
+//de layout activity_splash.xml.
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+//esse código aguarda 2 segundos e, em seguida, inicia a atividade PedidoActivity,
+//transferindo os dados da intenção atual para a nova atividade.
         Handler(Looper.getMainLooper()).postDelayed({
             val i = intent
             val j = Intent(this,PedidoActivity::class.java)
